4dc8f5a3cc94b63e8b7d34c2e6cfaafc529679a2
prajeeta pal <96904203+prajeeta15@users.noreply.github.com>
2025-07-01 16:28:04 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-
+const User = require("./models/user");
 const path = require("path");
 const mongoose = require("mongoose");
 const fs = require("fs");
